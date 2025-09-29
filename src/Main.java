29cfd8add1fe0e25a6f04b952e915751f3608ecf
anastv4 <anastazjafil.163@gmail.com>
2025-09-29 16:33:40 +0200

    losowanie i wypisywanie z tablicy i powtorzenia

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -16,11 +17,34 @@ public class Main {
 
         //wypisz wylosowane liczba na ekranie
 
+        //losowanie 10 liczb bez powtorzen
+        ArrayList<Integer> wylosowane = wylosujLiczbyDoListy(10);
+        wypisz(wylosowane);
+    }
+    private static ArrayList<Integer> wylosujLiczbyDoListy(int ileLiczb)   {
+        //kolekcje -> Listy Zbiory Map
+        //List -> ArrayList
+        //w kazdej kolekcji nie musimy deklarowac rozmiaru na poczatku pracy z kolekcja
+        //rozmiar moze byc zmieniany  -> dodawania i usuwania elementow
+        //w kolekcji ty;lko typy zlozone
+        int liczba1 = 0;//typ prosty z malej litery nie ma metod
+        Integer liczba2 = 0;// typ zlozony na metody pisane z wielkiej litery
+        Random random = new Random();
+        ArrayList<Integer> listaLiczbLosowych = new ArrayList<>();
+        for (int i = 0; i < ileLiczb; i++) {
+            listaLiczbLosowych.add(random.nextInt(20)+1);
+        }
+        return listaLiczbLosowych;
+    }
+    private static void wypisz(ArrayList<Integer> lista){
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.print(lista.get(i)+", ");
+        }
     }
     private static void wypisz(int[] tablica){
         System.out.println("Wypisywanie tablicy");
         for (int elementTablicy:tablica){
-            System.out.println(elementTablicy+" ,");
+            System.out.print(elementTablicy+" ,");
         }
     }
 
